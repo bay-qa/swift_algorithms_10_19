98b2b8ad1e1a509b6a6d2c0f5201569d4a2c8b90
Jayadeep Guttikonda <JayadeepGuttikonda@Nagas-MacBook-Air-2.local>
2019-10-20 08:59:30 -0700

    HW1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,34 @@ import UIKit
 /*
  1. Create a function that takes an integer and returns a boolean which indicates if the number is prime
  2. (optional for extra bonus) Create a function that takes an integer and prints all prime numbers before this integer */
+func isPrime(_ value:Int) -> Bool{
+    
+    var result = true
+    for i in 2 ..< value {
+        
+        if value % i == 0{
+            return false
+
+        }
+    }
+    return result
+}
+
+print(isPrime(11))
+
+
+//2. (optional for extra bonus) Create a function that takes an integer and prints all prime numbers before this integer */
+
+
+func printPrimes(to n: Int) -> [Int] {
+    var primes = [Int]()
+
+    for n in 2...n {
+        if primes.allSatisfy({ n % $0 != 0 }) {
+            primes.append(n)
+        }
+    }
+    return primes
+}
+
+print(printPrimes(to: 130))
